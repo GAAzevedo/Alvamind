3e1b75af55529932118600c2b56fb98089fee352
martinsroberto <robertomartinsramm@gmail.com>
2023-11-09 17:03:48 -0300

    bugs

--- src/components/Presenca/Area4Presenca.jsx
+++ src/components/Presenca/Area4Presenca.jsx
@@ -22,8 +22,8 @@ const Area4Presenca = () => {
 
         <div className="bg-gradient-to-b from-zinc-500 from-10% via-white dark:via-zinc-950 to-transparent p-0.5 rounded-xl items-center justify-center flex">
           <div className="grid sm:grid-cols-3 gap-5 p-5 rounded-xl bg-white dark:bg-zinc-950">
-            {texts.map((item) => (
-              <div data-aos="fade-up" data-aos-delay="500" className="relative mt-20 sm:mt-0">
+            {texts.map((item, index) => (
+              <div key={index} data-aos="fade-up" data-aos-delay="500" className="relative mt-20 sm:mt-0">
                 <div className=" flex w-full justify-center items-center mb-4 absolute -top-14">
                   <span className=" dark:text-white text-zinc-950 text-2xl dark:bg-zinc-950 bg-white p-4">{item.id}</span>
                 </div>
